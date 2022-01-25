1ae3d6a55467f0b0ecb23c5871b1ab776d0cc37c
Ivan Grishaev <ivan@grishaev.me>
2022-01-25 17:03:00 +0300

    updated

--- trash.clj
+++ trash.clj
@@ -23,3 +23,23 @@
                                       :post :handler.api/create-user}
 
                       ]}]}
+
+(ns ave.interceptor.util)
+
+
+(defn handler->interceptor
+  [handler]
+  {:enter
+   (fn [ctx]
+     (assoc ctx :response (handler ctx)))})
+
+
+(def interceptor-leave-response
+  {:leave :response})
+
+
+(defn wrap-stack
+  [interceptors handler]
+  (-> [interceptor-leave-response]
+      (into interceptors)
+      (conj (handler->interceptor handler))))
