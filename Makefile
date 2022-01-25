bb5b002093edbf0aaca8bcc910495312888fff76
Ivan Grishaev <ivan@grishaev.me>
2022-01-25 12:53:11 +0300

    updated

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 
 sub-install:
 	lein sub install
+
+repl-ave-core:
+	lein sub -s ave-core with-profile +test repl
--- ave-core/src/ave/system/main.clj
+++ ave-core/src/ave/system/main.clj
@@ -15,8 +15,10 @@
 (defonce ^:private
   -system (atom nil))
 
-(defn- -set-system [system]
-  (reset! -system system))
+
+(defn- -set-system [config]
+  (reset! -system (ig/init config)))
+
 
 (defn- -get-system []
   @-system)
@@ -36,7 +38,7 @@
            config]
 
     (let [fn-cmp
-          (fn [key1 key1]
+          (fn [key1 key2]
             (compare
              (get order key1 Integer/MAX_VALUE)
              (get order key2 Integer/MAX_VALUE)))]
@@ -81,6 +83,39 @@
       -to-ordered-map))
 
 
+(defn add-signals [config]
+
+  (signal/with-handler :term
+    (log/infof "Caught SIGTERM, quitting")
+    (ig/halt! (-get-system))
+    (log/infof "The system has been stopped")
+    (System/exit 0))
+
+  (signal/with-handler :int
+    (log/info "Caught SIGINT, quitting")
+    (ig/halt! (-get-system))
+    (log/infof "The system has been stopped")
+    (System/exit 0))
+
+  (signal/with-handler :hup
+    (log/info "Caught SIGHUP, restarting")
+    (ig/halt! (-get-system))
+    (-set-system config)
+    (log/info "The system has been restarted")))
+
+
+(defn -load-namespaces [config]
+
+  (let [ns-syms
+        (ig/load-namespaces config)]
+
+    (log/infof "Loaded namespaces: %s"
+               (with-out-str
+                 (println)
+                 (doseq [sym ns-syms]
+                   (println " -" sym))))))
+
+
 (defn main
 
   [{:as options
@@ -93,42 +128,15 @@
 
     (let [config
           (or config
-              (load-config options))
+              (load-config options))]
 
-          ns-syms
-          (ig/load-namespaces config)]
+      (-load-namespaces config)
 
-      (log/infof "Loaded namespaces: %s"
-                 (with-out-str
-                   (println)
-                   (doseq [sym ns-syms]
-                     (println " -" sym))))
-
-      ;; hack
-      (ig/init
-       (select-keys config [:ave.logging.unilog/ig]))
-
-      (-set-system (ig/init config))
+      (-set-system config)
 
       (log/infof "The system has been started")
 
-      (signal/with-handler :term
-        (log/infof "Caught SIGTERM, quitting")
-        (ig/halt! (-get-system))
-        (log/infof "The system has been stopped")
-        (System/exit 0))
-
-      (signal/with-handler :int
-        (log/info "Caught SIGINT, quitting")
-        (ig/halt! (-get-system))
-        (log/infof "The system has been stopped")
-        (System/exit 0))
-
-      (signal/with-handler :hup
-        (log/info "Caught SIGHUP, restarting")
-        (ig/halt! (-get-system))
-        (-set-system (ig/init config))
-        (log/info "The system has been restarted"))
+      (add-signals config)
 
       nil)
 
